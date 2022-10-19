61c92ce06378006bea3cbeff1b29f5c6ad0d3b4a
donghj2000 <84324399+donghj2000@users.noreply.github.com>
2022-10-19 09:14:13 +0800

    Update actionCreators.js

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -1,31 +1,43 @@
-import { SET_USER, CLEAR_USER } from "./constants.js";
+import { SET_USER, CLEAR_USER, SET_COMMENTPARAMS} from "./constants.js";
 
 export const setUseraction = (user) => {
-	return {
-		type: SET_USER,
-		user,
-	};
+    return {
+        type: SET_USER,
+        user,
+    };
 };
 
 export const clearUseraction = () => {
-	return {
-		type: CLEAR_USER
-	};
+    return {
+        type: CLEAR_USER
+    };
+};
+
+export const setComParamsaction = (comParams) => {
+    return {
+        type: SET_COMMENTPARAMS,
+        comParams,
+    };
 };
 
 export const mapStateToProps = (state) => {
-	return {
-		storeUser: state.user,
-	}
+    return {
+        storeUser: state.user,
+        storeComParams: state.comParams
+    }
 };
+
 export const mapDispatchToProps = (dispatch) => {
-	return {
-		setStoreuser: (user) => {
-			dispatch(setUseraction(user));
-		},
-		clearStoreuser: () => {
-			dispatch(clearUseraction());
-		}
-	}
+    return {
+        setStoreuser: (user) => {
+            dispatch(setUseraction(user));
+        },
+        clearStoreuser: () => {
+            dispatch(clearUseraction());
+        },
+        setStoreComParams: (comParams) => {
+            dispatch(setComParamsaction(comParams))
+        }
+    }
 }
 
