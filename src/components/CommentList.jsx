d3b2337c3083762f5d51acf0e210984ee91ce64b
donghj2000 <84324399+donghj2000@users.noreply.github.com>
2022-10-19 09:05:41 +0800

    Update CommentList.jsx

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -3,9 +3,11 @@ import Comment from "./Comment.jsx";
 import { timestampToTime } from "../utils";
 import { getArticleComments } from "../api/service";
 import { Input, Button, Modal, message } from "antd";
-import store from "./../store/index.js";
+import { connect } from 'react-redux';
+import { mapStateToProps, mapDispatchToProps } from './../store/actionCreators.js';
 import "antd/dist/antd.css";
 import "./CommentList.less";
+import ReplyTree from "./ReplyTree.jsx";
 
 class CommentList extends Component {
     constructor(props) {
@@ -20,7 +22,7 @@ class CommentList extends Component {
     }
 
     handleCancel() {
-        this.setState({ visible: false });
+        this.props.setStoreComParams({commentId: 0, commentVisible: false});
     }
 
     async getArticleCommentList() {
@@ -36,27 +38,8 @@ class CommentList extends Component {
     }
 
     async handleOk() {
-        this.setState({ visible: false });
+        this.props.setStoreComParams({commentId: 0, commentVisible: false});
         await this.getArticleCommentList();
-    }
-
-    showCommentModal(commentId, user, secondUser) {
-        if (store.getState().user.id === 0) {
-            message.warning("登录才能评论，请先登录!");
-            return;
-        }
-        if (!store.getState().user.is_active) {
-            message.warning("请先激活账号!");
-            return;
-        }
-        // 添加三级评论(暂无)
-        // if (secondUser) {
-        //     this.setState({comment_id: commentId});
-        // } else {
-            //添加二级评论
-            this.setState({comment_id: commentId});
-        //}
-        this.setState({ visible: true });
     }
 
     componentDidMount() {
@@ -71,76 +54,15 @@ class CommentList extends Component {
             </div>
 
             {
-                this.state.comments.map(item=>{
-                return (
-                    <div className="item" key={item.id}>
-                        <div className="item-header">
-                            <div className="author">
-                                <div className="avatar">
-                                    {
-                                        item.user_info.avatar?
-                                        (<img src={ item.user_info.avatar } alt="默认图片"  />):
-                                        (<img src="../assets/avatar.png"  alt="" />)
-                                    }
-                                </div>
-                            </div>
-                            <div className="info">
-                                <div className="name">
-                                    { item.user_info.name }
-                                    { item.user_info.role === "Admin"?"(作者)":"" }
-                                </div>
-                                <div className="time">{ timestampToTime(item.created_at, true) }</div>
-                            </div>
-                        </div>
-                        <div className="comment-detail">{ item.content }</div>
-                        <div className="item-comment">
-                            <div className="message"> 
-                                <Button type="primary" 
-                                    onClick={ ()=>this.showCommentModal(item.id, item.user_info.id) }>
-                                    回复    
-                                </Button>   
-                            </div>
-                        </div>
-                        {
-                            item.comment_replies.map(e=>{
-                            return (
-                                <div className="item-other" key={ e.id }>
-                                    <div className="item-header">
-                                        <div className="author">
-                                            <div className="avatar">
-                                            {
-                                                e.user_info.avatar?
-                                                (<img src={ e.user_info.avatar } alt="默认图片" />):
-                                                (<img src="../assets/avatar.png" alt="" />)
-                                            }
-                                            </div>
-                                        </div>
-                                        <div className="info">
-                                            <div className="name">
-                                                { e.user_info.name }
-                                                { e.user_info.role === "Admin"?"(作者)":"" }
-                                            </div>
-                                            <div className="time">
-                                                { timestampToTime(e.created_at, true) }
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="comment-detail">
-                                        { e.content }
-                                    </div>
-                                </div>
-                                )
-                            })
-                        }
-                    </div>) 
-                })
+                this.state.comments.length > 0?
+                (<ReplyTree replies={ this.state.comments } level={0} />):""
             } 
 
             <Comment 
                 articleId={ this.props.articleId }
                 forArticle={ false }
-                reply={ this.state.comment_id }
-                showDialog={ this.state.visible }
+                reply={ this.props.storeComParams.commentId }
+                showDialog={ this.props.storeComParams.commentVisible }
                 onCancel={ this.handleCancel.bind(this) }
                 onOk={ this.handleOk.bind(this) }
             />
@@ -148,4 +70,5 @@ class CommentList extends Component {
       )
     }
 }
-export default CommentList;
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentList);
